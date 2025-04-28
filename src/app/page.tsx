421f21b40da451b5adf3291b5299c37fab40b7dc
Jonathanleivag <email@jonathanleivag.cl>
2025-04-28 01:51:32 -0400

    Add HeroComponent to homepage layout
    Introduce a new `HeroComponent` with animations, styling, and layout enhancements, replacing the simple "Hello World" text. This improves the visual design and provides a more engaging entry point on the homepage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import HeroComponent from "@/components/hero/hero.component";
+
 export default function Home() {
     return (
-        <h1>Hello world</h1>
+        <>
+            <HeroComponent/>
+        </>
     );
 }
