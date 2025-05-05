79c9636390e5b7697f21fb5b0d941c9827bf9223
Jonathanleivag <email@jonathanleivag.cl>
2025-05-04 23:58:16 -0400

    Add toast notifications, enhance middleware, and types
    Integrated `react-hot-toast` for user feedback with toast notifications. Refactored the middleware to include token revalidation and cookie handling. Updated types, environment variables, and utility functions for improved environment management and strict typing. Removed unnecessary HOCs and streamlined layout components.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,19 @@
 import {ReactNode} from "react";
 import "./globals.css";
 import StoreProvider from "@/lib/redux/StoreProvider";
+import {Toaster} from "react-hot-toast";
 
 export default function RootLayout({children,}: Readonly<{
     children: ReactNode;
 }>) {
     return (
-        <html lang="es">
-        <body className='min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900'>
-        <StoreProvider>
-            {children}
-        </StoreProvider>
-        </body>
-        </html>
+            <html lang="es">
+            <body className='min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900'>
+            <StoreProvider>
+                 <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
+                {children}
+            </StoreProvider>
+            </body>
+            </html>
     );
 }
