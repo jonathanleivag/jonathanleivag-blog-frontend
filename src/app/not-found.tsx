96495299511899ad5213d5886b22b81f8599865c
Jonathanleivag <email@jonathanleivag.cl>
2025-05-20 00:16:46 -0400

    Add metadata to 404 page
    Define a metadata object with a title for the 404 page to improve clarity and consistency. This ensures better handling by search engines and aligns with the blog's branding.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,9 @@
 import NotFound from "@/components/page/not-found";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: '404 - Página no encontrada | Blog Jonathanleivag',
+}
 
 const NotFoundPage = () => {
     return <NotFound/>;
