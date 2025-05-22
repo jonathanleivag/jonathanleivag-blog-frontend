c3d91667d9a95d47019a66a1a62befcf0e0b7654
Jonathanleivag <email@jonathanleivag.cl>
2025-05-22 06:58:41 -0400

    sum view

--- src/app/api/blog/view/[slug]/route.ts
+++ src/app/api/blog/view/[slug]/route.ts
@@ -2,7 +2,7 @@ import {NextRequest, NextResponse} from "next/server";
 import {getEnv} from "@/utils/getEnv.util";
 import {ENV} from "@/enum";
 
-export async function GET(req: NextRequest,  { params }: { params: Promise<{ slug: string }> },) {
+export async function GET(req: NextRequest, {params}: { params: Promise<{ slug: string }> },) {
     try {
         const url = getEnv(ENV.BACKEND_URL);
         const response = await fetch(`${url}/blog/view/${(await params).slug}`, {
@@ -12,11 +12,32 @@ export async function GET(req: NextRequest,  { params }: { params: Promise<{ slu
             },
         });
         const data = await response.json();
-        return NextResponse.json(data, { status: 200 });
+        return NextResponse.json(data, {status: 200});
     } catch (error) {
         if (error instanceof Error) {
-            return NextResponse.json({ error: error.message }, { status: 500 });
+            return NextResponse.json({error: error.message}, {status: 500});
         }
-        return NextResponse.json({ error: "Fetch failed" }, { status: 500 });
+        return NextResponse.json({error: "Fetch failed"}, {status: 500});
+    }
+}
+
+
+export async function PATCH(req: NextRequest, {params}: { params: Promise<{ slug: string }> },) {
+    try {
+        const slug = (await params).slug;
+        const url = getEnv(ENV.BACKEND_URL)
+        const response = await fetch(`${url}/blog/view/${slug}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        const data = await response.json()
+        return NextResponse.json(data, {status: 200});
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json({error: error.message}, {status: 500});
+        }
+        return NextResponse.json({error: "Fetch failed"}, {status: 500});
     }
 }
